5dd25fa12108aa3d4eb13afe7b747f8d4fc40cdd
Oskar <oskarcole@gmail.com>
2023-12-13 15:20:29 -0800

    Update DriveConstants.java
    SMiley

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -48,7 +48,7 @@ public class DriveConstants {
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
     //public static double TRACK_WIDTH = 11.7525; // in
     public static double TRACK_WIDTH = 12.75; // in
-
+//:)
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
      * the built-in velocity PID, *these values are fine as is*. However, if you do not have drive
